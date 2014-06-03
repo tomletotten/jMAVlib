0d243e65483125a77e0d6546347ae2e9ed69814b
Anton Babushkin <anton.babushkin@me.com>
2014-06-03 16:34:06 +0200

    MAVLink: CRC calculation optimized

--- src/me/drton/jmavlib/mavlink/MAVLinkCRC.java
+++ src/me/drton/jmavlib/mavlink/MAVLinkCRC.java
@@ -15,31 +15,9 @@ public class MAVLinkCRC {
      * @return the new accumulated checksum
      */
     public static int accumulateCRC(byte data, int crc) {
-        // TODO optimize
-        int tmp, tmpdata;
-        int crcaccum = crc & 0x000000ff;
-        tmpdata = data & 0x000000ff;
-        tmp = tmpdata ^ crcaccum;
-        tmp &= 0x000000ff;
-        int tmp4 = tmp << 4;
-        tmp4 &= 0x000000ff;
-        tmp ^= tmp4;
-        tmp &= 0x000000ff;
-        int crch = crc >> 8;
-        crch &= 0x0000ffff;
-        int tmp8 = tmp << 8;
-        tmp8 &= 0x0000ffff;
-        int tmp3 = tmp << 3;
-        tmp3 &= 0x0000ffff;
-        tmp4 = tmp >> 4;
-        tmp4 &= 0x0000ffff;
-        int tmpa = crch ^ tmp8;
-        tmpa &= 0x0000ffff;
-        int tmpb = tmp3 ^ tmp4;
-        tmpb &= 0x0000ffff;
-        crc = tmpa ^ tmpb;
-        crc &= 0x0000ffff;
-        return crc;
+        int tmp = (data ^ crc) & 0xff;
+        tmp ^= (tmp << 4) & 0xff;
+        return ((crc >> 8) ^ (tmp << 8) ^ (tmp << 3) ^ (tmp >> 4)) & 0xffff;
     }
 
     public static int calculateCRC(byte[] data) {
